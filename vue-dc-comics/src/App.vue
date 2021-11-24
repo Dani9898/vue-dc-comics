d3a5460ca0218fe8a4a4e0dbaf3b8ff2f4b8ad8f
Dani9898 <danieleoliva98@gmail.com>
2021-11-24 14:36:56 +0100

    componenti creati

---
+++ vue-dc-comics/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import ContenutoPrincipale from './components/ContenutoPrincipale.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    ContenutoPrincipale,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
