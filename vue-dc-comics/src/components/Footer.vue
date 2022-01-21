b28339448318ccdb6b93b29f46114d9341765008
Dani9898 <danieleoliva98@gmail.com>
2022-01-21 15:52:04 +0100

    feat: footer

--- vue-dc-comics/src/components/Footer.vue
+++ vue-dc-comics/src/components/Footer.vue
@@ -110,6 +110,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
 #footer-buy {
   height: 150px;
   background-color: #0282f9;
